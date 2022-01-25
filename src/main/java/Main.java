1dc959d598add4b7963bda8d273da39829995b94
marcosimperatori <marcosimperatori@gmail.com>
2022-01-24 21:53:15 -0300

    commit inicial

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        Cliente marcos = new Cliente();
+        marcos.setNome("Marcos");
+
+        Conta cc = new ContaCorrente(marcos);
+        Conta poupanca = new ContaPoupanca(marcos);
+
+        cc.depositar(100);
+        cc.transferir(100, poupanca);
+
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+    }
+
+}
